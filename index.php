eeca63875b912fd609450ae4b48345700304f1ab
laviRZ <laviarzi@gmail.com>
2019-06-26 19:53:08 +0300

    sending the login request to dbread.php

--- index.php
+++ index.php
@@ -11,16 +11,16 @@
 </head>
 <!-- login form-->
 <form name="login" id="loginform" action="login.php" method="post">
-    <input name="username" placeholder="Your username here:"><br>
-    <input name="password" type="password" placeholder="Your password here:"><br>
-    <input class="formBtn" name="loginBtn" type="submit" value="Log In">
+    <input name="lusername" placeholder="Your username here:"><br>
+    <input name="lpassword" type="password" placeholder="Your password here:"><br>
+    <input class="formBtn" name="loginBtn" type="button" value="Log In" onclick="request(true)">
     <button type="button" id="dntHaveAcntBtn" onclick="replace()">Not registered yet?</button>
 </form>
 <!-- register form -->    
 <form name="register" id="registerform" action="register.php" method="post">
-    <input name="username" placeholder="Your full name here:"><br>
-    <input name="email" placeholder="Your email address here:"><br>
-    <input name="password" type="password" placeholder="********"><br>
+    <input name="rusername" placeholder="Your full name here:"><br>
+    <input name="remail" placeholder="Your email address here:"><br>
+    <input name="rpassword" type="password" placeholder="********"><br>
     <input class="formBtn" name="regBtn" type="submit" value="Register">
     <button type="button" id="haveAcntBtn" onclick="replace()">Already have an account?</button>
 </form>
--- mainscript.js
+++ mainscript.js
@@ -16,3 +16,19 @@ function replace() {
         registerform.style.display = "initial";
     }
 }
+
+function request(log){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            //this.responseText;
+        }
+    };
+    var data = new Array();
+    if(log){
+        data[0] = document.getElementsByName('lusername')[0].value;
+        data[1] = document.getElementsByName('lpassword')[0].value;
+    }
+    xmlhttp.open("POST", "dbread.php", false);
+    xmlhttp.send(data);
+}
